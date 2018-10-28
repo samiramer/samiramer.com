198fc3e1ed35e5fcebbd9fd43382907088b363a1
Samir <samirmamer@gmail.com>
2018-10-28 15:58:03 -0400

    Update gatsby config with proper names

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'CodeLoop',
+        short_name: 'CodeLoop',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
