13a87ef59c3460d14f033e443873d04b5b93b7a5
Samir <samirmamer@gmail.com>
2018-11-09 13:56:53 -0500

    Add some text to the contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,10 +5,19 @@ import Layout from '../components/Layout/layout'
 
 const ProcessPage = () => (
   <Layout>
-    <div className="max-w-lg mx-auto px-8 flex">
+    <div className="max-w-lg mx-auto px-8 flex flex-col my-auto text-center text-grey-darkest leading-loose">
       <h1 className="text-2xl md:text-4xl text-center text-grey-darkest">
-        Coming Soon ...
+        The contact form is coming soon!
       </h1>
+      <h2 className="mt-8">
+        But I am always available via email at
+        <br />
+        <a className="text-red" href="mailto:samir@codeloop.co">
+          &lt;samir@codeloop.co&gt;
+        </a>
+        <br />
+        Let's build something!
+      </h2>
       {/* <ContactUs /> */}
     </div>
   </Layout>
