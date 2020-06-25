7d410c7d60648fb4507cd9fc178907d98938d974
samiramer <samiramer@users.noreply.github.com>
2020-06-25 19:29:12 -0400

    Update footer.js

--- src/components/Layout/footer.js
+++ src/components/Layout/footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
     <div className="bg-red-light">
         <div className="flex justify-center max-w-xl mx-auto p-8 xl:p-12">
             <p className="text-lg text-center text-grey-lightest">
-                Let's chat!  You can reach me at <a className="text-white font-bold" href="mailto:sam@codeloop.co">&lt;samir@codeloop.co&gt;</a>
+                Let's chat!  You can reach me at <a className="text-white font-bold" href="mailto:sam@codeloop.co">&lt;sam@codeloop.co&gt;</a>
             </p>
         </div>
     </div>
