7879b9cc2ed38620c62c2ed730ea7787eea3fe93
Samir Amer <samirmamer@gmail.com>
2022-12-07 14:01:30 -0500

    Implement GTM

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -27,19 +27,11 @@ const Layout = ({ children }) => (
             { name: 'keywords', content: data.site.siteMetadata.title },
           ]}
         >
-          <script 
-            async 
-            src="https://www.googletagmanager.com/gtag/js?id=G-07C5ZFGLRM"
-          />
-          <script>
-            {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-07C5ZFGLRM');
-            `}
-          </script>
+          <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M3CH35D');`}</script>
           <html lang="en" />
         </Helmet>
         <div className="flex flex-col min-h-screen">
